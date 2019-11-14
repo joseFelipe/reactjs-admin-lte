f3895333b095010f7c38e3544552163c5597aa84
Felipe Silva <felipe.florianopolis@gmail.com>
2019-11-13 23:25:41 -0300

    Simple application using Admin LTE theme with ReacJS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Sidebar />
+        <Footer />
+      </>
+    );
+  }
 }
-
-export default App;
